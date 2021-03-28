a14dbf734cffdcabb2a4f1232e3bfdf27c240400
Glenn Joe Paul <glenn.paul@knolskape.com>
2021-03-28 13:29:42 +0530

    v1.0 ready

--- app.js
+++ app.js
@@ -3,12 +3,9 @@ const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
-const { google } = require('googleapis');
 const cors = require('cors');
-const urlParse = require('url-parse');
-const queryParse = require('query-string');
+
 const bodyParser = require('body-parser');
-const axios = require('axios');
 
 const GOOGLE_CLIENT_ID = '691744154791-sqq6djn27k6vou8jqmb19bjr6p9pp4b3.apps.googleusercontent.com';
 const GOOGLE_CLIENT_SECRET = '_VQeqxq1DB0LUaY-dQNIanl1';
@@ -16,13 +13,12 @@ const GOOGLE_CLIENT_SECRET = '_VQeqxq1DB0LUaY-dQNIanl1';
 const app = express();
 const port = process.env.PORT || 3000;
 
-
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use(morgan('tiny'));
-app.use(express.static(path.join(__dirname,'/public')));
+app.use(express.static(path.join(__dirname, '/public')));
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
@@ -31,26 +27,23 @@ const verifyroute = require('./src/routes/verifyroute.js')(GOOGLE_CLIENT_ID, GOO
 app.use('/auth', verifyroute);
 
 app.get('/', (req, res) => {
-  res.render('contacts');
+  res.render('index');
 });
 
 app.listen(port, () => {
   debug(`Server listening on port  ${chalk.green(port)}`);
 });
 
-
-
-
-  // service.people.createContact({
-  //   requestBody: {
-  //     emailAddresses: [{value: 'test@test.com'}],
-  //     names: [
-  //       {
-  //         displayName: 'A',
-  //         familyName: 'B',
-  //         givenName: 'C',
-  //       },
-  //     ],
-  //   },
-  // });
-  // //console.log('\n\nCreated Contact:', newContact);
+// service.people.createContact({
+//   requestBody: {
+//     emailAddresses: [{value: 'test@test.com'}],
+//     names: [
+//       {
+//         displayName: 'A',
+//         familyName: 'B',
+//         givenName: 'C',
+//       },
+//     ],
+//   },
+// });
+// //console.log('\n\nCreated Contact:', newContact);
--- src/routes/verifyroute.js
+++ src/routes/verifyroute.js
@@ -3,51 +3,26 @@ const { google } = require('googleapis');
 const axios = require('axios');
 const urlParse = require('url-parse');
 const queryParse = require('query-string');
+const { firebaserules } = require('googleapis/build/src/apis/firebaserules');
 
 const verifyroute = express.Router();
 
-function listConnectionNames(oath) {
-  const service = google.people({version: 'v1', auth:oath });
-  service.people.connections.list({
-    resourceName: 'people/me',
-    pageSize: 3,
-    personFields: 'names,phoneNumbers',
-  }, (err, res) => {
-    if (err) return console.error(`The API returned an error: ${err}`);
-    const { connections } = res.data;
-    if (connections) {
-      connections.forEach((person) => {
-        if (person.names && person.names.length > 0) {
-          console.log(person.names[0].displayName);
-        }
-      });
-    } else {
-      console.log('No connections found.');
-    }
-  });
-  service.people.get({
-    resourceName: 'people/me',
-    personFields: 'names,emailAddresses',
-  }, (err, res) => {
-    if (err) return console.error(`The API returned an error: ${err}`);
-    const per = res.data;
-    const profinfo = { name: per.names[0].displayName, email:per.emailAddresses[0].value};
-    console.log(profinfo.name,profinfo.email);
-  });
-};
+let profinfo = 1;
+let pingo = [];
+let oath2 = false;
 
 function router(GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET) {
   verifyroute.route('/')
     .get((req, res) => {
-      const oath2 = new google.auth.OAuth2(
+      const oaths2 = new google.auth.OAuth2(
         GOOGLE_CLIENT_ID,
         GOOGLE_CLIENT_SECRET,
-        'http://localhost:3000/auth/contacts',
+        'http://localhost:3000/auth/contacts/',
       );
 
-      const SCOPES = ['https://www.googleapis.com/auth/contacts', 'profile'];
+      const SCOPES = ['https://www.googleapis.com/auth/contacts', 'profile', 'email'];
 
-      const url = oath2.generateAuthUrl({
+      const url = oaths2.generateAuthUrl({
         access_type: 'offline',
         scope: SCOPES,
         state: JSON.stringify({
@@ -63,21 +38,84 @@ function router(GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET) {
           console.log(error);
         });
     });
+
   verifyroute.route('/contacts')
     .get(async (req, res) => {
-      const queryURL = new urlParse(req.url);
-      const { code } = queryParse.parse(queryURL.query);
+      if (oath2 === false) {
+        const queryURL = new urlParse(req.url);
+        const { code } = queryParse.parse(queryURL.query);
 
-      const oath2 = new google.auth.OAuth2(
-        GOOGLE_CLIENT_ID,
-        GOOGLE_CLIENT_SECRET,
-        'http://localhost:3000/auth/contacts',
-      );
+        oath2 = new google.auth.OAuth2(
+          GOOGLE_CLIENT_ID,
+          GOOGLE_CLIENT_SECRET,
+          'http://localhost:3000/auth/contacts/',
+        );
+        const { tokens } = await oath2.getToken(code);
+        oath2.setCredentials(tokens);
+      }
+      // listConnectionNames(oath2);
+      const service = google.people({ version: 'v1', auth: oath2 });
+      const promise1 = new Promise((resolve, reject) => {
+        service.people.get({
+          resourceName: 'people/me',
+          personFields: 'names,emailAddresses,photos',
+        }, (err, res) => {
+          if (err) {
+            console.error(`The API returned an error: ${err}`);
+            reject();
+          }
+          const per = res.data;
+          profinfo = {
+            name: per.names[0].displayName,
+            email: per.emailAddresses[0].value,
+            photo: per.photos[0].url,
+          };
+          console.log(profinfo.name, profinfo.email);
+          resolve();
+        });
+      });
 
-      const { tokens } = await oath2.getToken(code);
-      oath2.setCredentials(tokens);
-      res.render('contacts');
-      listConnectionNames(oath2);
+      const promise2 = new Promise((resolve, reject) => {
+        service.people.connections.list({
+          resourceName: 'people/me',
+          pageSize: 500,
+          personFields: 'names,phoneNumbers,photos',
+        }, (err, res) => {
+          if (err) return console.error(`The API returned an error: ${err}`);
+          const { connections } = res.data;
+          if (connections) {
+            let i = 0;
+            connections.forEach((person) => {
+              if (person.names && person.names.length > 0 && person.phoneNumbers) {
+                pingo[i++] = {
+                  name: person.names[0].displayName,
+                  phone: person.phoneNumbers[0].canonicalForm,
+                  photo: person.photos[0].url,
+                };
+              }
+            });
+            resolve();
+          } else {
+            console.log('No connections found.');
+            reject();
+          }
+        });
+      });
+
+      Promise.all([promise1, promise2]).then(() => {
+        res.redirect('/auth/view');
+      });
+    });
+  verifyroute.route('/view')
+    .get((req, res) => {
+      res.render('contacts copy', { pingo, profinfo });
+    });
+  verifyroute.route('/logout')
+    .get((req, res) => {
+      oath2 = false;
+      pingo = [];
+      profinfo = 1;
+      res.redirect('/');
     });
 
   return verifyroute;
